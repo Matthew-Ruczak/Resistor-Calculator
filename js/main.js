4ca7fc4d4c7d2eab02f7c436e65599b51151b90c
Matthew-Ruczak <47807679+Matthew-Ruczak@users.noreply.github.com>
2020-05-29 17:10:38 -0500

    Added basic CSS.

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ let element_text_tolerance;
 let element_text_tempCoefficent;
 
 window.onload = ()=> {
+    //Getting reference to all of the bands select elements
     element_band_1 = document.getElementById('band_1');
     element_band_2 = document.getElementById('band_2');
     element_band_3 = document.getElementById('band_3');
@@ -18,13 +19,28 @@ window.onload = ()=> {
     element_band_multiplier = document.getElementById('band_multiplier');
     element_band_tolerance = document.getElementById('band_tolerance');
     element_band_tempCoefficient = document.getElementById('band_tempCoefficient');
+
+    //Setting up event handlers for the band select elements so that when they change, the information about the resistor is updated
+    element_band_1.addEventListener("change", updateDisplay);
+    element_band_2.addEventListener("change", updateDisplay);
+    element_band_3.addEventListener("change", updateDisplay);
+    element_band_multiplier.addEventListener("change", updateDisplay);
+    element_band_tolerance.addEventListener("change", updateDisplay);
+    element_band_tempCoefficient.addEventListener("change", updateDisplay);
+
+    //Getting reference to the areas were the information about the resistors will be updated
     element_text_ohms = document.getElementById('text_ohms');
     element_text_tolerance = document.getElementById('text_tolerance');
     element_text_tempCoefficent = document.getElementById('text_tempCoefficent');
+
+    //Setting up default information about the resistor
+    updateDisplay();
 }
 
 function getNumOfOhms(){
+    //Concating the numbers together
     let ohmsStr = element_band_1.value + element_band_2.value + element_band_3.value;
+    //Applying the multiplier
     ohmsStr = parseInt(ohmsStr) * parseFloat(element_band_multiplier.value);
     return ohmsStr;
 }
@@ -34,13 +50,17 @@ function getTolerance(){
 }
 
 function getTempCoefficient(){
-    return element_band_tempCoefficient.value;
+    const {value} = element_band_tempCoefficient;
+    if (value === "N/A")
+        return ".";
+    return ", and a temperature coefficient of " + value + "ppm.";
 }
 
 function updateDisplay(){
     let numOfOhms = getNumOfOhms();
     let tolerance = getTolerance();
     element_text_ohms.innerHTML = numOfOhms;
+    //Displaying the tolerance percentage and the actual ± value
     element_text_tolerance.innerHTML = tolerance + '% (±' + numOfOhms * (tolerance / 100) + ' Ohms)';
     element_text_tempCoefficent.innerHTML = getTempCoefficient();
 }
